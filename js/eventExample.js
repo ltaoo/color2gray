8b3422fb3cc484c8723267d599470e50ff1ed9af
ltaoo <litaowork@aliyun.com>
2019-04-19 21:31:04 +0800

    chore: 清理无关代码

---
+++ js/eventExample.js
@@ -0,0 +1,64 @@
+var EVENT_ID_MAP = {
+  /** 
+    {
+      "eventID":1936028772,
+      "eventData":{
+          "null":{
+            "_property":"foregroundColor",
+            "_ref":"color"
+          },
+          "source":"eyeDropperSample",
+          "to":{
+            "_obj":"RGBColor",
+            "blue":127.004,
+            "grain":185.996,
+            "red":245
+          }
+      }
+    }
+    */
+  1936028772: '取色器取色',
+  /** 
+    {
+      "eventID":1936028772,
+      "eventData":{
+          "null":{
+            "_property":"foregroundColor",
+            "_ref":"color"
+          },
+          "source":"colorPickerPanel | photoshopPicker",
+          "to":{
+            "_obj":"HSBColorClass",
+            "brightness":19.0005,
+            "hue":{
+                "_unit":"angleUnit",
+                "_value":115.999
+            },
+            "saturation":70.0008
+          }
+      }
+    }
+    */
+  1936028772: '取色面板取色',
+  /** 
+    {
+      "eventID": 1936028772,
+      "eventData": {
+        "null": {
+          "_property": "foregroundColor",
+          "_ref": "color"
+        },
+        "source": "colorPickerRamp",
+        "to": {
+          "_obj": "CMYKColorClass",
+          "black": 0.78,
+          "cyan": 100,
+          "magenta": 77.26,
+          "yellowColor": 11.37
+        }
+      }
+    }
+    */
+};
+
+
