6ccff212ede0b12915c4ff13f6b05325b2f3f090
litao <tli66@best-inc.com>
2019-04-26 15:41:59 +0800

    doc: 增加 windows 下的安装说明

--- windows-installer.bat
+++ windows-installer.bat
@@ -1,2 +1,18 @@
-if exist "C:\Program Files\Common Files\Adobe\CEP\extensions" explorer "C:\Program Files\Common Files\Adobe\CEP\extensions"
-if exist "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions" explorer "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions"
+@title 自动安装 photoshop 插件
+@echo off
+if exist "C:\Program Files\Common Files\Adobe\CEP\extensions" (
+	REM explorer "C:\Program Files\Common Files\Adobe\CEP\extensions"
+	echo "plugin is exist"
+	.\unzip\bin\unzip.exe -o .\color2gray.zxp -d "C:\Program Files\Common Files\Adobe\CEP\extensions\color2gray"
+	echo "插件安装成功，请重启 PS"
+)
+if exist "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions" (
+	REM explorer "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions"
+	echo "plugin is exist"
+	REM "%rar%" e color2gray.zxp "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\color2gray"
+	.\unzip\bin\unzip.exe -o .\color2gray.zxp -d "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\color2gray"
+	echo install success, please restart photoshop
+)
+
+pause
+@echo on
